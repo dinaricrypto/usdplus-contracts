2f72cd0abbe42b13517acb00c977a60324bc0cf3
jaketimothy <jake.a.timothy@gmail.com>
2023-10-23 16:57:53 -0700

    feat: setup project

---
+++ src/usd+.sol
@@ -0,0 +1,6 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.21;
+
+/// @notice stablecoin
+/// @author Dinari (https://github.com/dinaricrypto/usdplus-contracts/blob/main/src/usd+.sol)
+contract usdplus {}
