5f428ed364fec41d8abb32a6b82e8ccab28ef079
jaketimothy <jake.a.timothy@gmail.com>
2023-10-23 17:19:19 -0700

    feat: base tokens

---
+++ src/usd++.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: GPL-3.0-or-later
+pragma solidity 0.8.21;
+
+import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
+import {ERC20Permit, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+/// @notice stablecoin yield vault
+/// @author Dinari (https://github.com/dinaricrypto/usdplus-contracts/blob/main/src/usd++.sol)
+contract usdplusplus is ERC4626, ERC20Permit {
+    constructor(IERC20 usdplus) ERC4626(usdplus) ERC20Permit("USD++") ERC20("USD++", "USD++") {}
+
+    function decimals() public view virtual override(ERC4626, ERC20) returns (uint8) {
+        return ERC4626.decimals();
+    }
+}
--- src/usd+.sol
+++ src/usd+.sol
@@ -1,6 +1,10 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
 pragma solidity 0.8.21;
 
+import {ERC20Permit, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
+
 /// @notice stablecoin
 /// @author Dinari (https://github.com/dinaricrypto/usdplus-contracts/blob/main/src/usd+.sol)
-contract usdplus {}
+contract usdplus is ERC20Permit {
+    constructor() ERC20("USD+", "USD+") ERC20Permit("USD+") {}
+}
