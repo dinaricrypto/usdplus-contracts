e7d754de447cca56c28b32fb2266784d240c6553
jaketimothy <jake.a.timothy@gmail.com>
2023-10-25 15:03:53 -0700

    feat: USD++ per account mint locks

--- src/UsdPlusPlus.sol
+++ src/UsdPlusPlus.sol
@@ -4,27 +4,38 @@ pragma solidity 0.8.21;
 import {ERC4626} from "@openzeppelin/contracts/token/ERC20/extensions/ERC4626.sol";
 import {ERC20Permit, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-// import {EnumerableSet} from "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
-
-import {TimeData} from "./TimeData.sol";
+import {DoubleEndedQueue} from "@openzeppelin/contracts/utils/structs/DoubleEndedQueue.sol";
 
 /// @notice stablecoin yield vault
 /// @author Dinari (https://github.com/dinaricrypto/usdplus-contracts/blob/main/src/usd++.sol)
 contract UsdPlusPlus is ERC4626, ERC20Permit {
+    using DoubleEndedQueue for DoubleEndedQueue.Bytes32Deque;
+
+    struct Lock {
+        uint48 endTime;
+        uint104 assets;
+        uint104 shares;
+    }
+
+    // pack into single slot
+    struct LockTotals {
+        uint128 assets;
+        uint128 shares;
+    }
 
     error ValueOverflow();
+    error LockTimeTooShort(uint256 wait);
+    error ValueLocked(uint256 freeValue);
 
     // TODO: continuous yield?
 
-    // TODO: view methods to get locked state
+    uint48 public lockDuration = 30 days;
 
-    uint40 public lockDuration = 30 days;
+    // dequeue of locks per account
+    // back == most recent lock
+    mapping(address => DoubleEndedQueue.Bytes32Deque) private _locks;
 
-    // if we pack the amount and endtime into a uint256, what is the max mint amount?
-
-    // locks need account, endtime, amount
-    // mapping(address => EnumerableSet.UintSet) private _locks;
-    // minheap of locks by endtime?
+    mapping(address => LockTotals) private _cachedLockTotals;
 
     constructor(IERC20 usdplus) ERC4626(usdplus) ERC20Permit("USD++") ERC20("USD++", "USD++") {}
 
@@ -34,44 +45,153 @@ contract UsdPlusPlus is ERC4626, ERC20Permit {
 
     // ------------------ Lock ------------------
 
-    function packLockData(uint48 time, uint104 assets, uint104 shares) internal pure returns (uint256) {
-        return uint256(time) << 208 | uint256(assets) << 104 | shares;
+    /// @notice locked USD++ for account
+    function sharesLocked(address account) external view returns (uint256) {
+        return _cachedLockTotals[account].shares;
     }
 
-    function unpackLockData(uint256 packed) internal pure returns (uint48, uint104, uint104) {
-        return (uint40(packed >> 208), uint104(packed >> 104), uint104(packed));
+    /// @notice complete lock schedule for account
+    function getLockSchedule(address account) external view returns (Lock[] memory) {
+        uint256 n = _locks[account].length();
+        Lock[] memory schedule = new Lock[](n);
+        for (uint256 i = 0; i < n; i++) {
+            schedule[i] = unpackLockData(_locks[account].at(i));
+        }
+        return schedule;
     }
 
-    // function addLock(address to, uint256 assets, uint256 shares) internal {
-    //     if (assets > typeof(uint108).max) revert ValueOverflow();
-    //     if (shares > typeof(uint108).max) revert ValueOverflow();
+    /// @dev pack lock data into a single bytes32
+    function packLockData(Lock memory lock) internal pure returns (bytes32) {
+        return bytes32(uint256(lock.endTime) << 208 | uint256(lock.assets) << 104 | lock.shares);
+    }
 
-    //     _locks[to].add(packLockData(uint40(block.timestamp) + lockDuration, uint108(assets), uint108(shares)));
-    // }
+    /// @dev unpack lock data from a single bytes32
+    function unpackLockData(bytes32 packed) internal pure returns (Lock memory) {
+        uint256 packedInt = uint256(packed);
+        return Lock(uint48(packedInt >> 208), uint104(packedInt >> 104), uint104(packedInt));
+    }
+
+    /// @dev add lock to heap and update cached totals
+    function addLock(address account, uint256 assets, uint256 shares) internal {
+        if (assets > type(uint104).max) revert ValueOverflow();
+        if (shares > type(uint104).max) revert ValueOverflow();
+
+        // TODO: reduce gas by not loading locktotals twice, not peeking twice
+        groomLockQueue(_locks[account], account);
+
+        // ensure new lock ends after previous lock
+        // this means that if lockDuration is changed, users may have to wait before they can mint more USD++
+        uint48 endTime = uint48(block.timestamp) + lockDuration;
+        if (_locks[account].length() > 0) {
+            Lock memory prevLock = unpackLockData(_locks[account].back());
+            if (endTime < prevLock.endTime) revert LockTimeTooShort(prevLock.endTime - endTime);
+        }
+        _locks[account].pushBack(packLockData(Lock(endTime, uint104(assets), uint104(shares))));
+        LockTotals memory cachedTotals = _cachedLockTotals[account];
+        _cachedLockTotals[account] =
+            LockTotals(uint128(cachedTotals.assets + assets), uint128(cachedTotals.shares + shares));
+    }
+
+    /// @dev remove expired locks and update cached totals
+    function groomLockQueue(DoubleEndedQueue.Bytes32Deque storage queue, address account) internal {
+        // remove expired locks
+        uint128 assetsDecrement;
+        uint128 sharesDecrement;
+        while (queue.length() > 0) {
+            Lock memory lock = unpackLockData(queue.front());
+            if (lock.endTime > block.timestamp) break;
+            assetsDecrement += lock.assets;
+            sharesDecrement += lock.shares;
+            queue.popFront();
+        }
+        if (assetsDecrement > 0) {
+            LockTotals memory cachedTotals = _cachedLockTotals[account];
+            _cachedLockTotals[account] =
+                LockTotals(cachedTotals.assets - assetsDecrement, cachedTotals.shares - sharesDecrement);
+        }
+    }
+
+    function consumeLocks(DoubleEndedQueue.Bytes32Deque storage locks, address account, uint256 shares)
+        internal
+        returns (uint128)
+    {
+        if (shares > type(uint128).max) revert ValueOverflow();
+
+        uint128 assetsDue;
+        uint128 sharesRemaining = uint128(shares);
+        while (sharesRemaining > 0) {
+            Lock memory lock = unpackLockData(locks.popFront());
+            if (lock.shares > sharesRemaining) {
+                // partially consume lock and return to queue
+                uint128 assets = uint128(lock.assets) * sharesRemaining / lock.shares;
+                assetsDue += assets;
+                sharesRemaining = 0;
+                locks.pushFront(
+                    packLockData(
+                        Lock(lock.endTime, uint104(lock.assets - assets), uint104(lock.shares - sharesRemaining))
+                    )
+                );
+            } else {
+                // fully consume lock
+                assetsDue += lock.assets;
+                sharesRemaining -= lock.shares;
+            }
+        }
+        LockTotals memory cachedTotals = _cachedLockTotals[account];
+        _cachedLockTotals[account] = LockTotals(cachedTotals.assets - assetsDue, cachedTotals.shares - uint128(shares));
+        return assetsDue;
+    }
 
     function _deposit(address caller, address receiver, uint256 assets, uint256 shares) internal virtual override {
-        // addLock(receiver, assets, shares);
+        // add lock on user mint
+        addLock(receiver, assets, shares);
         super._deposit(caller, receiver, assets, shares);
     }
 
-    // on redeem, any locked amount is exchanged at original price
+    function _withdraw(address caller, address receiver, address owner, uint256 assets, uint256 shares)
+        internal
+        virtual
+        override
+    {
+        groomLockQueue(_locks[owner], owner);
 
-    // function totalLockedShares(address account) external view returns (uint256) {
-    //     uint256[] memory locks = _locks[account].values();
-    //     uint256 total = 0;
-    //     for (uint256 i = 0; i < locks.length; i++) {
-    //         (uint40 endTime,, uint256 shares) = unpackLockData(locks[i]);
-    //         // if lock has not expired, add shares
-    //         if (endTime > block.timestamp) {
-    //             total += shares;
-    //         }
-    //     }
-    //     return total;
-    // }
+        // if locked shares, determine how many assets to withdraw
+        uint256 assetsToWithdraw;
+
+        // check if enough free shares
+        uint128 lockedShares = _cachedLockTotals[owner].shares;
+        if (lockedShares == 0) {
+            // if no locked shares, withdraw at normal rate
+            assetsToWithdraw = assets;
+        } else {
+            uint256 freeShares = balanceOf(owner) - lockedShares;
+            if (shares > freeShares) {
+                if (freeShares > 0) {
+                    // if burning free shares, withdraw at normal rate
+                    assetsToWithdraw += assets * freeShares / shares;
+                }
+                // if burning locked shares, redeem for original USD+
+                assetsToWithdraw += consumeLocks(_locks[owner], owner, shares - freeShares);
+            } else {
+                // if not burning locked shares, withdraw at normal rate
+                assetsToWithdraw = assets;
+            }
+        }
+
+        // widthdraw assets
+        super._withdraw(caller, receiver, owner, assetsToWithdraw, shares);
+    }
 
     function _update(address from, address to, uint256 value) internal virtual override {
-        // TODO: transfer lock on recently minted USD++, except when burning for original USD+
-        if (from != address(0) && to != address(0)) {}
+        // transfer lock on recently minted USD++, minting and burning handled in _deposit and _withdraw
+        if (from != address(0) && to != address(0)) {
+            groomLockQueue(_locks[from], from);
+
+            // revert if not enough free shares
+            uint256 freeShares = balanceOf(from) - _cachedLockTotals[from].shares;
+            if (value > freeShares) revert ValueLocked(freeShares);
+        }
+
         super._update(from, to, value);
     }
 
