60e4e4100e19413ca18ca4be24c5ac94da601415
jaketimothy <jake.a.timothy@gmail.com>
2023-11-13 12:34:21 -0800

    feat: solc 0.8.23

--- src/mocks/ERC20Mock.sol
+++ src/mocks/ERC20Mock.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0-or-later
-pragma solidity 0.8.21;
+pragma solidity 0.8.23;
 
 import {ERC20Permit, ERC20} from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Permit.sol";
 import {AccessControl} from "@openzeppelin/contracts/access/AccessControl.sol";
