176531c59962cd7893e6dbd4596bc13cb0c2bc5a
jaketimothy <jake.a.timothy@gmail.com>
2023-12-28 13:01:56 -0800

    chore: remove old scripts

--- script/grantrole-sepolia.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp .env-sepolia .env
-source .env
-
-forge script script/GrantRole.s.sol:GrantRole --rpc-url $RPC_URL -vvv --broadcast
--- script/upgradebridge-sepolia.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp .env-sepolia .env
-source .env
-
-forge script script/UpgradeCCIPBridge.s.sol:UpgradeCCIPBridge --rpc-url $RPC_URL -vvv --broadcast --verify
