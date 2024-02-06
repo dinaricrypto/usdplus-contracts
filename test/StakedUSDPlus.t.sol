5f81ec74122462d844054f09c744f4dc5dda4bf0
jaketimothy <jake.a.timothy@gmail.com>
2024-02-05 16:12:33 -0800

    chore: gas report

--- test/StakedUSDPlus.t.sol
+++ test/StakedUSDPlus.t.sol
@@ -141,6 +141,30 @@ contract StakedUsdPlusTest is Test {
         assertEq(stakedusdplus.getLockSchedule(address(USER)).length, 9);
     }
 
+    function test_refreshManyLocks() public {
+        usdplus.mint(USER, 10000);
+
+        vm.startPrank(USER);
+        usdplus.approve(address(stakedusdplus), 10000);
+        // TODO: test double entry
+        // stakedusdplus.deposit(10, USER);
+        uint256 intialtime = block.timestamp;
+        for (uint256 i = 0; i < 100; i++) {
+            stakedusdplus.deposit(10, USER);
+            vm.warp(intialtime + 1 + i);
+        }
+        vm.stopPrank();
+
+        // refresh locks - not expired
+        stakedusdplus.refreshLocks(USER);
+        assertEq(stakedusdplus.getLockSchedule(address(USER)).length, 100);
+
+        // refresh locks - expired
+        vm.warp(block.timestamp + 30 days);
+        stakedusdplus.refreshLocks(USER);
+        assertEq(stakedusdplus.getLockSchedule(address(USER)).length, 0);
+    }
+
     function test_mintLocks(uint104 amount1, uint104 amount2) public {
         vm.assume(stakedusdplus.previewDeposit(amount1) > 0);
         vm.assume(stakedusdplus.previewDeposit(amount2) > 0);
