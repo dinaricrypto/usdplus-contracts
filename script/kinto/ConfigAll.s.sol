6bb5df7243c118911a91d76c03e2ffd7872f0ecc
joshualyguessennd <75019812+joshualyguessennd@users.noreply.github.com>
2024-06-28 19:57:39 +0000

    DSHARE-165 Update Kinto Config.s.all (#58)
    * update configAll script
    
    * add comment for permission to call
    
    * style: fmt
    
    ---------
    
    Co-authored-by: jaketimothy <jake.a.timothy@gmail.com>

--- script/kinto/ConfigAll.s.sol
+++ script/kinto/ConfigAll.s.sol
@@ -6,11 +6,10 @@ import {TransferRestrictor} from "../../src/TransferRestrictor.sol";
 import {UsdPlus} from "../../src/UsdPlus.sol";
 import {UsdPlusMinter} from "../../src/UsdPlusMinter.sol";
 import {UsdPlusRedeemer} from "../../src/UsdPlusRedeemer.sol";
-// import {AggregatorV3Interface} from "chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 import {IERC20} from "openzeppelin-contracts/contracts/token/ERC20/IERC20.sol";
 import {ERC20Mock} from "../../src/mocks/ERC20Mock.sol";
 import {IKintoWallet} from "kinto-contracts-helpers/interfaces/IKintoWallet.sol";
-
+import {AccessControl} from "openzeppelin-contracts/contracts/access/AccessControl.sol";
 import "kinto-contracts-helpers/EntryPointHelper.sol";
 import "kinto-contracts-helpers/AASetup.sol";
 
@@ -24,9 +23,8 @@ contract ConfigAll is AASetup, EntryPointHelper {
         address operator2;
         IERC20 usdc;
     }
-    // AggregatorV3Interface usdcOracle;
 
-    IEntryPoint _entryPoint;
+    IEntryPoint private _entryPoint;
 
     function setUp() public {
         (, _entryPoint,,) = _checkAccountAbstraction();
@@ -34,7 +32,6 @@ contract ConfigAll is AASetup, EntryPointHelper {
     }
 
     function run() external {
-        // load env variables
         uint256 deployerPrivateKey = vm.envUint("DEPLOYER_KEY");
         address deployer = vm.addr(deployerPrivateKey);
         address owner = vm.envAddress("OWNER");
@@ -48,75 +45,109 @@ contract ConfigAll is AASetup, EntryPointHelper {
             operator2: vm.envAddress("OPERATOR2"),
             usdc: IERC20(vm.envAddress("USDC"))
         });
-        // usdcOracle: AggregatorV3Interface(vm.envAddress("USDC_ORACLE"))
 
         console.log("deployer: %s", deployer);
         console.log("owner: %s", owner);
 
-        // send txs as deployer
         vm.startBroadcast(deployerPrivateKey);
 
-        // authorize kinto wallet to call contracts
-        address[] memory apps = new address[](5);
-        apps[0] = address(cfg.transferRestrictor);
-        apps[1] = address(cfg.usdplus);
-        apps[2] = address(cfg.minter);
-        apps[3] = address(cfg.redeemer);
-        apps[4] = address(cfg.usdc); //MockUSDC
+        _grantRoles(cfg, owner, deployerPrivateKey);
+        _setIssuerLimits(cfg, owner, deployerPrivateKey);
+        _grantERC20Roles(cfg, owner, deployerPrivateKey);
 
-        bool[] memory flags = new bool[](5);
-        flags[0] = true;
-        flags[1] = true;
-        flags[2] = true;
-        flags[3] = true;
-        flags[4] = true;
+        vm.stopBroadcast();
+    }
 
-        _handleOps(
-            _entryPoint,
-            abi.encodeWithSelector(IKintoWallet.whitelistApp.selector, apps, flags),
+    function _grantRoles(Config memory cfg, address owner, uint256 deployerPrivateKey) internal {
+        // Grant RESTRICTOR_ROLE to operators
+        // permissions to call
+        // - restrict(address account)
+        // - unrestrict(address account)
+        _grantRole(
+            address(cfg.transferRestrictor),
+            cfg.transferRestrictor.RESTRICTOR_ROLE(),
+            cfg.operator,
             owner,
+            deployerPrivateKey
+        );
+        _grantRole(
+            address(cfg.transferRestrictor),
+            cfg.transferRestrictor.RESTRICTOR_ROLE(),
+            cfg.operator2,
             owner,
             deployerPrivateKey
         );
 
-        // permissions to call
-        // - restrict(address account)
-        // - unrestrict(address account)
-        cfg.transferRestrictor.grantRole(cfg.transferRestrictor.RESTRICTOR_ROLE(), cfg.operator);
-        cfg.transferRestrictor.grantRole(cfg.transferRestrictor.RESTRICTOR_ROLE(), cfg.operator2);
-
+        // Grant OPERATOR_ROLE to operators
         // permissions to call
         // - rebaseAdd(uint128 value)
         // - rebaseMul(uint128 factor)
-        cfg.usdplus.grantRole(cfg.usdplus.OPERATOR_ROLE(), cfg.operator);
-        cfg.usdplus.grantRole(cfg.usdplus.OPERATOR_ROLE(), cfg.operator2);
-        // permissions to call
-        // - mint(address to, uint256 value)
-        cfg.usdplus.setIssuerLimits(address(cfg.minter), type(uint256).max, 0);
-        // permissions to call
-        // - burn(address from, uint256 value)
-        // - burn(uint256 value)
-        cfg.usdplus.setIssuerLimits(address(cfg.redeemer), 0, type(uint256).max);
+        _grantRole(address(cfg.usdplus), cfg.usdplus.OPERATOR_ROLE(), cfg.operator, owner, deployerPrivateKey);
+        _grantRole(address(cfg.usdplus), cfg.usdplus.OPERATOR_ROLE(), cfg.operator2, owner, deployerPrivateKey);
 
-        // cfg.minter.setPaymentTokenOracle(cfg.usdc, cfg.usdcOracle);
-
+        // Grant FULFILLER_ROLE to operators
         // permissions to call
         // - fulfill(uint256 ticket)
         // - cancel(uint256 ticket)
-        cfg.redeemer.grantRole(cfg.redeemer.FULFILLER_ROLE(), cfg.operator);
-        cfg.redeemer.grantRole(cfg.redeemer.FULFILLER_ROLE(), cfg.operator2);
-        // cfg.redeemer.setPaymentTokenOracle(cfg.usdc, cfg.usdcOracle);
+        _grantRole(address(cfg.redeemer), cfg.redeemer.FULFILLER_ROLE(), cfg.operator, owner, deployerPrivateKey);
+        _grantRole(address(cfg.redeemer), cfg.redeemer.FULFILLER_ROLE(), cfg.operator2, owner, deployerPrivateKey);
+    }
+
+    function _setIssuerLimits(Config memory cfg, address owner, uint256 deployerPrivateKey) internal {
+        // Set issuer limits for minter and redeemer
+        // permissions to call
+        // - mint(address to, uint256 value)
+        // permissions to call
+        // - burn(address from, uint256 value)
+        // - burn(uint256 value)
+        _setIssuerLimit(cfg.usdplus, address(cfg.minter), type(uint256).max, 0, owner, deployerPrivateKey);
+        _setIssuerLimit(cfg.usdplus, address(cfg.redeemer), 0, type(uint256).max, owner, deployerPrivateKey);
+    }
+
+    function _grantERC20Roles(Config memory cfg, address owner, uint256 deployerPrivateKey) internal {
+        // Grant roles for ERC20Mock (USDC)
+        ERC20Mock mockUSDC = ERC20Mock(address(cfg.usdc));
 
         // permissions to call
         // - mint(address to, uint256 value)
-        // - burnFrom(address from, uint256 value)
-        // - burn(uint256 value)
-        ERC20Mock mockUSDC = ERC20Mock(address(cfg.usdc));
-        mockUSDC.grantRole(mockUSDC.MINTER_ROLE(), cfg.operator);
-        mockUSDC.grantRole(mockUSDC.MINTER_ROLE(), cfg.operator2);
-        mockUSDC.grantRole(mockUSDC.BURNER_ROLE(), cfg.operator);
-        mockUSDC.grantRole(mockUSDC.BURNER_ROLE(), cfg.operator2);
+        // - burn(address from, uint256 value)
+        _grantRole(address(mockUSDC), mockUSDC.MINTER_ROLE(), cfg.operator, owner, deployerPrivateKey);
+        _grantRole(address(mockUSDC), mockUSDC.MINTER_ROLE(), cfg.operator2, owner, deployerPrivateKey);
+        _grantRole(address(mockUSDC), mockUSDC.BURNER_ROLE(), cfg.operator, owner, deployerPrivateKey);
+        _grantRole(address(mockUSDC), mockUSDC.BURNER_ROLE(), cfg.operator2, owner, deployerPrivateKey);
+    }
 
-        vm.stopBroadcast();
+    function _grantRole(
+        address contractAddress,
+        bytes32 role,
+        address account,
+        address owner,
+        uint256 deployerPrivateKey
+    ) internal {
+        AccessControl contractAccessControl = AccessControl(contractAddress);
+        _handleOps(
+            _entryPoint,
+            abi.encodeWithSelector(contractAccessControl.grantRole.selector, role, account),
+            owner,
+            contractAddress,
+            deployerPrivateKey
+        );
+    }
+
+    function _setIssuerLimit(
+        UsdPlus usdplus,
+        address account,
+        uint256 mintLimit,
+        uint256 burnLimit,
+        address owner,
+        uint256 deployerPrivateKey
+    ) internal {
+        _handleOps(
+            _entryPoint,
+            abi.encodeWithSelector(usdplus.setIssuerLimits.selector, account, mintLimit, burnLimit),
+            owner,
+            address(usdplus),
+            deployerPrivateKey
+        );
     }
 }
