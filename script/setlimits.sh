6bde8572ff0404dd3791f48cf0af36d73fc2d714
Jake <jake.a.timothy@gmail.com>
2024-07-10 22:02:31 -0700

    chore: fill redemption request script (#63)
    * chore: fill redemption request script
    
    * chore: add redeemer to config owner script

--- script/setlimits.sh
+++ script/setlimits.sh
@@ -1,21 +1,6 @@
 #!/bin/sh
 
-# cp .env-ethereum .env
-# source .env
-
-# forge script script/SetMintBurnLimits.s.sol:SetMintBurnLimits --rpc-url $RPC_URL -vvv --broadcast
-
-# cp .env-arbitrum .env
-# source .env
-
-# forge script script/SetMintBurnLimits.s.sol:SetMintBurnLimits --rpc-url $RPC_URL -vvv --broadcast
-
 cp .env-arbitrum-sepolia .env
-source .env
-
-forge script script/SetMintBurnLimits.s.sol:SetMintBurnLimits --rpc-url $RPC_URL -vvv --broadcast
-
-cp .env-sepolia .env
 source .env
 
 forge script script/SetMintBurnLimits.s.sol:SetMintBurnLimits --rpc-url $RPC_URL -vvv --broadcast
