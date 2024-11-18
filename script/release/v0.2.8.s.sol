3f73a18f7ef528cd00fac1b49b3622eec844199b
jaketimothy <jake.a.timothy@gmail.com>
2024-11-17 23:24:52 -0800

    chore: release workflow

---
+++ script/release/v0.2.8.s.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.23;
+
+import "forge-std/Script.sol";
+
+contract Upgrade is Script {
+
+    function run() external {
+        console.log("Upgrade to v0.2.8");
+    }
+}
