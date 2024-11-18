fb5f1730d68ffa679e3c9a2b1b912dee8e30eb77
jaketimothy <jake.a.timothy@gmail.com>
2024-11-18 12:13:01 -0800

    chore: set oracle script

--- script/deployoracle.sh
+++ script/deployoracle.sh
@@ -1,6 +1,3 @@
 #!/bin/sh
 
-cp .env-sandbox .env
-source .env
-
-forge create src/mocks/UnityOracle.sol:UnityOracle --rpc-url $RPC_URL --private-key $DEPLOYER_KEY --verify
+op run --env-file="./.env-ethereum" -- ./script/deployoracle-cmd.sh
