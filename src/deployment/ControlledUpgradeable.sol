674f08f4be63d15285cd982e779f0bd7f9847bbf
joshualyguessennd <josue@dinari.com>
2024-12-23 09:28:41 +0000

    abstract contract controlled upgradeable

---
+++ src/deployment/ControlledUpgradeable.sol
@@ -0,0 +1,11 @@
+import {UUPSUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/proxy/utils/UUPSUpgradeable.sol"; 
+import {AccessControlDefaultAdminRulesUpgradeable} from "openzeppelin-contracts-upgradeable/contracts/access/extensions/AccessControlDefaultAdminRulesUpgradeable.sol";
+
+abstract contract ControlledUpgradeable is UUPSUpgradeable, AccessControlDefaultAdminRulesUpgradeable {
+    /// ------------------ Types ------------------ ///
+    bytes32 public constant UPGRADER_ROLE = keccak256("UPGRADER_ROLE");
+
+    function _authorizeUpgrade(address implementation) internal override onlyRole(UPGRADER_ROLE) {}
+
+    function version() external pure returns (string memory) {}
+}
