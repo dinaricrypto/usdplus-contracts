b85597799a0e990093807a3270630a36e5b4a070
Jake <jake.a.timothy@gmail.com>
2025-01-17 15:32:06 -0800

    v0.2.9 testnet deployments (#116)

---
+++ script/upgrade029-cmd.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+forge script script/Upgrade_028_029.s.sol:Upgrade_028_029 --rpc-url $RPC_URL -vvv --broadcast --slow --verify
---
+++ script/upgrade029.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+op run --env-file="./.env-sepolia" -- ./script/upgrade029-cmd.sh
+
+op run --env-file="./.env-arbitrum-sepolia" -- ./script/upgrade029-cmd.sh
+
+op run --env-file="./.env-base-sepolia" -- ./script/upgrade029-cmd.sh
+
+op run --env-file="./.env-sandbox" -- ./script/upgrade029-cmd.sh
+
+# op run --env-file="./.env-arbitrum" -- ./script/upgrade029-cmd.sh
+
+# op run --env-file="./.env-ethereum" -- ./script/upgrade029-cmd.sh
+
+# op run --env-file="./.env-base" -- ./script/upgrade029-cmd.sh
+
+# op run --env-file="./.env-kinto-prod" -- ./script/upgrade029-orbit-cmd.sh
