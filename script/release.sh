957baf812bc68b4f92df2b213ea27edc89620446
joshualyguessennd <josue@dinari.com>
2025-01-31 19:45:06 +0000

    remove data

--- script/release.sh
+++ script/release.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-RPC_URL="https://sepolia.infura.io/v3/0a7b42115f6a48c0b2aa5be4aacfd789"
-VERIFY_URL="https://sepolia.infura.io/v3/0a7b42115f6a48c0b2aa5be4aacfd789"
-PRIVATE_KEY="0xd518574e456daf683bdfd4f85666b3a4de3eac9a014e675347a556fc365c9557"
-ETHERSCAN_API_KEY="6W3SFV585FASFJDXG47MSSR4H97AT54TK9"
+RPC_URL=""
+VERIFY_URL=""
+PRIVATE_KEY=""
+ETHERSCAN_API_KEY=""
 
 export VERSION="1.0.0" # 1.0.0
 export ENVIRONMENT="staging" #staging, production
