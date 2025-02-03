c08f83ac8f48abf55142c4cad764a0ac72bdddd2
joshualyguessennd <josue@dinari.com>
2025-02-03 22:37:26 +0000

    add env comment

--- script/release.sh
+++ script/release.sh
@@ -1,26 +1,30 @@
 #!/bin/sh
-# RPC_URL - mandatory
-# VERIFY_URL - optional
-# PRIVATE_KEY - mandatory
-# ETHERSCAN_API_KEY - optional
 
-# export VERSION="" # 1.0.0 - mandatory
-# export ENVIRONMENT="" #[staging, production] - mandatory
+# Required environment variables:
+# ENVIRONMENT         - Target environment ["staging", "production"]
+# RPC_URL            - RPC endpoint URL
+# PRIVATE_KEY        - Deploy private key
+#
+# Optional:
+# VERIFY_URL         - Contract verifier URL (if not using Etherscan)
+# ETHERSCAN_API_KEY  - Etherscan API key
+# DEPLOYED_VERSION   - Version of the previous deployment
 
+export VERSION="1.0.0" # Version of current deployment
 
 CONTRACTS=("usdplus" "transfer_restrictor" "usdplus_minter" "usdplus_redeemer" "ccip_waypoint")
 
 for i in "${CONTRACTS[@]}"; do
-    echo "Deploying $i"
-    export CONTRACT=$i
-    
-    FORGE_CMD="forge script script/Release.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast -vvv"
-    
-    if [ ! -z "$ETHERSCAN_API_KEY" ] || [ ! -z "$VERIFIER_URL" ]; then
-        FORGE_CMD="$FORGE_CMD --verify"
-    fi
-    
-    eval $FORGE_CMD || echo "Failed to deploy $i, continuing..."
+   echo "Deploying $i"
+   export CONTRACT=$i
+   
+   FORGE_CMD="forge script script/Release.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast -vvv"
+   
+   if [ ! -z "$ETHERSCAN_API_KEY" ] || [ ! -z "$VERIFIER_URL" ]; then
+       FORGE_CMD="$FORGE_CMD --verify"
+   fi
+   
+   eval $FORGE_CMD || echo "Failed to deploy $i, continuing..."
 done
 
 echo "Deployment process completed."
