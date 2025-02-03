56c8bf7ef1e4356185244f1cfb767fa718f02bf7
joshualyguessennd <josue@dinari.com>
2025-02-03 18:22:44 +0000

    add if condition to verify contract

--- script/release.sh
+++ script/release.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-RPC_URL=""
-VERIFY_URL=""
-PRIVATE_KEY=""
-ETHERSCAN_API_KEY=""
+# RPC_URL - mandatory
+# VERIFY_URL - optional
+# PRIVATE_KEY - mandatory
+# ETHERSCAN_API_KEY - optional
 
-export VERSION="1.0.0" # 1.0.0
-export ENVIRONMENT="staging" #staging, production
+# export VERSION="" # 1.0.0 - mandatory
+# export ENVIRONMENT="" #[staging, production] - mandatory
 
 
 CONTRACTS=("usdplus" "transfer_restrictor" "usdplus_minter" "usdplus_redeemer" "ccip_waypoint")
@@ -13,12 +13,14 @@ CONTRACTS=("usdplus" "transfer_restrictor" "usdplus_minter" "usdplus_redeemer" "
 for i in "${CONTRACTS[@]}"; do
     echo "Deploying $i"
     export CONTRACT=$i
-    forge script script/Release.s.sol \
-    --rpc-url $RPC_URL \
-    --private-key $PRIVATE_KEY \
-    --verify \
-    --broadcast \
-    -vvv || echo "Failed to deploy $i, continuing..."
+    
+    FORGE_CMD="forge script script/Release.s.sol --rpc-url $RPC_URL --private-key $PRIVATE_KEY --broadcast -vvv"
+    
+    if [ ! -z "$ETHERSCAN_API_KEY" ] || [ ! -z "$VERIFIER_URL" ]; then
+        FORGE_CMD="$FORGE_CMD --verify"
+    fi
+    
+    eval $FORGE_CMD || echo "Failed to deploy $i, continuing..."
 done
 
 echo "Deployment process completed."
