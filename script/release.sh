08c0fd24a8c7ec2ceff907e0fb7476df19ab5841
Young Kim <young@dinari.com>
2025-02-19 22:11:05 -0800

    Fix a typo in comparison

--- script/release.sh
+++ script/release.sh
@@ -26,7 +26,7 @@ for i in "${CONTRACTS[@]}"; do
     FORGE_CMD="$FORGE_CMD --verify --delay 10 --retries 30"
   fi
 
-  if ["$CHAIN_ID" == "98864"]; then
+  if [ "$CHAIN_ID" == "98864" ]; then
     FORGE_CMD="$FORGE_CMD --legacy --skip-simulation"
   fi
 
